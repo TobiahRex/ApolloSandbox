90159a22b50f2eef94fcde1458541c734c9120c2
TobiahRex <tobiah.rex@gmail.com>
2017-05-15 15:47:20 +0900

    created channelList.js

---
+++ client/channelList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './resources/logo.svg';
+import './App.css';
+
+const ChannelList = () => (
+  <ul>
+    <li>Channel 1</li>
+    <li>Channel 2</li>
+  </ul>
+);
+export default ChannelList;
