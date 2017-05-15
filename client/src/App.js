9e2272edf9d68f61e193c34122b07e248adef5c3
TobiahRex <tobiah.rex@gmail.com>
2017-05-15 17:12:47 +0900

    creatd mockNetworkInterface.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ChannelList from './channelList';
-
 import {
   ApolloClient,
   ApolloProvider,
 } from 'react-apollo';
+import {
+  makeExecutableSchema,
+  addMockFunctionsToSchema,
+} from 'graphql-tools';
+import { mockNetworkInterfaceWithSchema } from 'apollo-test-utils';
+import { typeDefs } from './schema';
 
-const client = new ApolloClient();
+import logo from './logo.svg';
+import './App.css';
+import ChannelList from './channelList';
+
+const schema = makeExecutableSchema({ typeDefs });
+addMockFunctionsToSchema({ schema });
+const mockNetworkInterface = mockNetworkInterfaceWithSchema({ schema });
+const client = new ApolloClient({
+  networkInterface: mockNetworkInterface,
+});
 
 class App extends Component {
   render() {
